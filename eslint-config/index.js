e434d1cc89b5d9065264f763c41ba1dc180f6815
xalpol12 <xalpol12@gmail.com>
2026-09-15 22:13:57 +0200

    gitignore

---
+++ eslint-config/index.js
@@ -0,0 +1,12 @@
+// index.js for @xalpol12/eslint-config
+export default {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@angular-eslint/recommended",
+    "plugin:@stylistic/recommended"
+  ],
+  rules: {
+    // Add your custom rules here
+  }
+};
+
---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+// eslint.config.js for ESLint v9+
+import xalpolConfig from "@xalpol12/eslint-config";
+
+export default [
+  xalpolConfig
+];
+
